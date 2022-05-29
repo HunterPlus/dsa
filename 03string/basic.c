a42f50097e30f1c5ce33e50efde3563fd228921a
HunterPlus <zlw08140957@gmail.com>
2022-05-29 13:28:18 +0800

    Update basic.c

--- 03string/basic.c
+++ 03string/basic.c
@@ -16,3 +16,27 @@ char *stringchr (const char *s, char c)
 		s++;
 	return s;
 }
+
+/* stringcpy - strcpy - copy a string */
+char *stringcpy (char *dest, const char *src)
+{
+	char *t = dest;
+
+	while ((*dest++ = *src++) != '\0') 
+		/* nothing */;	
+	return t;
+}
+
+/* stringncpy - strncpy - most n bytes of src are copied */
+char *stringncpy(char *dest, const char *src, size_t n)
+{
+	char	*t = dest;
+	
+	while (n) {
+		if ((*t = *src) != '\0')
+			src++;
+		t++;
+		n--;
+	}
+	return dest;
+}
