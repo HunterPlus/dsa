de00bd937f41c29ab01116c4ae868809d66267b0
HunterPlus <zlw08140957@gmail.com>
2022-05-30 09:42:07 +0800

    Update basic.c

--- 04matrix/basic.c
+++ 04matrix/basic.c
@@ -21,7 +21,20 @@ void math()
 		for (int j = 0; j < 3; j++)
 			c[i][j] *= scale;	
 }
-
+void mul()
+{
+	int a[2][3] = { 1, 2, 3, 4, 5, 6 };
+	int b[3][4] = { {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+	int c[2][4] = {0};
+	int i, j, k;
+	
+	for (i = 0; i < 2; i++)
+		for (j = 0; j < 4; j++)
+			for (k = 0; k < 3; k++)
+				c[i][j] += a[i][k] * b[k][j];
+				
+	
+}
 int main()
 {
 	int arr[2][3] = { 11, 12, 13, 21, 22, 23 };
