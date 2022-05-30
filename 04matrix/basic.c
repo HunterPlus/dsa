65ff87ec91d616bba6ac985aab56e7e85d4924e0
HunterPlus <zlw08140957@gmail.com>
2022-05-30 09:28:26 +0800

    Update basic.c

--- 04matrix/basic.c
+++ 04matrix/basic.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 
-void add()
+void math()
 {
 	int a[2][3] = { 1, 2, 3, 4, 5, 6 };
 	int b[2][3] = { 7, 8, 9, 10, 11, 12 };
 	int c[2][3];
 	
+	/* add - add two matrix */
 	for (int i = 0; i < 2; i++)
 		for (int j = 0; j < 3; j++)
 			c[i][j] = a[i][j] + b[i][j];
+	/* bias */
+	int bias = 3;
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 3; j++)
+			c[i][j] += bias;
+	/* scale */
+	int scale = 2;
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 3; j++)
+			c[i][j] *= scale;	
 }
 
 int main()
