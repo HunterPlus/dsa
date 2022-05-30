77a9ac207b2067df33cfb651c46a1fe8072561e4
HunterPlus <zlw08140957@gmail.com>
2022-05-30 09:12:05 +0800

    Update basic.c

--- 04matrix/basic.c
+++ 04matrix/basic.c
@@ -4,9 +4,9 @@ int main()
 {
   	int arr[2][3] = { 11, 12, 13, 21, 22, 23 };    
    	for (int i = 0; i < 2; i++) {
-       		for (int j = 0; j < 3; j++)
-     			printf("%3d  ", arr[i][j]);
-    		printf("\n");
+   		for (int j = 0; j < 3; j++)
+  			printf("%3d  ", arr[i][j]);
+  		printf("\n");
    	}
    	return 0;
 }
