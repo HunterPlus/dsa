bd739c207044e99ff5cea7d845b645c21b705aba
HunterPlus <zlw08140957@gmail.com>
2022-05-31 05:59:12 +0800

    Update arrayof.c

--- 04matrix/arrayof.c
+++ 04matrix/arrayof.c
@@ -3,23 +3,23 @@
 
 int scmp(const void *p1, const void *p2)
 {
-    	const char *s1, *s2;
+        const char *s1, *s2;
         
-    	s1 = *(char **)p1;
-    	s2 = *(char **)p2;
-    	return strcmp(s1, s2);
+        s1 = *(char **)p1;
+        s2 = *(char **)p2;
+        return strcmp(s1, s2);
 }
 
 int main()
 {
-    	char *name[] = {"Thompson", "Ritchie", "Pike", "Johnson"};
-    	int     n = sizeof(name) / sizeof(name[0]);
+        char *name[] = {"Thompson", "Ritchie", "Pike", "Johnson"};
+        int     n = sizeof(name) / sizeof(name[0]);
         
-    	for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
                 printf("%s\n", name[i]);
         printf("\n");
         qsort(name, n, sizeof name[0], scmp);
-    	for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
                 printf("%s\n", name[i]);        
-    	return 0;
+        return 0;
 }
