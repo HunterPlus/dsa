d06c5e5a31f1e795c03cf4d80255965b6ff9744c
HunterPlus <zlw08140957@gmail.com>
2022-05-30 09:16:40 +0800

    Update basic.c

--- 04matrix/basic.c
+++ 04matrix/basic.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+void add()
+{
+	int a[2][3] = { 1, 2, 3, 4, 5, 6 };
+	int b[2][3] = { 7, 8, 9, 10, 11, 12 };
+	int c[2][3];
+	
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 3; j++)
+			c[i][j] = a[i][j] + b[i][j];
+}
+
 int main()
 {
 	int arr[2][3] = { 11, 12, 13, 21, 22, 23 };
