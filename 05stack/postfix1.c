9a4f1110a8188efecc54f89fbdfd723ac8469e86
HunterPlus <zlw08140957@gmail.com>
2022-05-31 10:43:04 +0800

    Update postfix1.c

--- 05stack/postfix1.c
+++ 05stack/postfix1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-
+#include "stack.h"
+	
 int postfix(char *exp)
 {
 	char	*p;
@@ -8,14 +9,17 @@ int postfix(char *exp)
 	
 	p = exp;
 	while (*p != '\0') {
-		if (p == ' ' || p == '\t')
+		if (*p == ' ' || *p == '\t')
 			p++;
 		if (*p >= '0' && *p <= '9') {
 			push(*p - '0');
 			p++;
 			continue;
 		}
-		
+		switch (*p) {
+		case '+': push(pop() + pop()); continue;
+				
+		}
 	}
 }
 
