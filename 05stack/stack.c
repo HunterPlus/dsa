4a4884093189bd74a71c83d9d762de86aad0538d
HunterPlus <zlw08140957@gmail.com>
2022-05-31 08:07:50 +0800

    Create stack.c

---
+++ 05stack/stack.c
@@ -0,0 +1,26 @@
+#incude <stdio.h>
+#include "stack.h"
+
+static int buf[BUFSIZ];
+static int *sp = buf;
+
+int empty()	/* if stack is empty return 1, otherwise 0 */
+{
+}
+
+int size()
+{
+	return BUFSIZ;
+}
+
+void push(int data)
+{
+}
+
+int pop()
+{
+}
+
+int top()
+{
+}
