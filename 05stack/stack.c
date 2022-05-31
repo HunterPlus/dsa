645903b18edf4c006a2bb3abc240f0da18b7396d
HunterPlus <zlw08140957@gmail.com>
2022-05-31 08:32:15 +0800

    Update stack.c

--- 05stack/stack.c
+++ 05stack/stack.c
@@ -1,11 +1,13 @@
 #incude <stdio.h>
+#include <limits.h>
 #include "stack.h"
 
-static int buf[BUFSIZ];
-static int *sp = buf;
+static int buf[BUFSIZ];		/* buffer of stack */
+static int sp = 0;		/* stack pointer, next free position */
 
-int empty()	/* if stack is empty return 1, otherwise 0 */
+int empty()			/* if stack is empty return 1, otherwise 0 */
 {
+	return sp == 0;
 }
 
 int size()
@@ -15,12 +17,18 @@ int size()
 
 void push(int data)
 {
+	if (sp >= BUFSIZ)
+		printf("push: stack is overflow");
+	else
+		buf[sp++] = data;
 }
 
 int pop()
 {
+	return (sp > 0) ? buf[--sp] : INT_MIN;
 }
 
 int top()
 {
+	return (sp > 0) ? buf[sp - 1] : INT_MIN;
 }
