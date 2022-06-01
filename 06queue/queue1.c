853048ff9e7b68583f59057a75bb7dcddc0e1115
HunterPlus <zlw08140957@gmail.com>
2022-06-01 08:03:26 +0800

    Update queue1.c

--- 06queue/queue1.c
+++ 06queue/queue1.c
@@ -10,7 +10,34 @@ int size()
 	return size;
 }
 
+int full()
+{
+	return size = BUFSIZ;
+}
+
 int empty()
 {
 	return size == 0;
 }
+
+void enqueue(int data)
+{
+	if (full())
+		return;
+	buf[rear] = data;
+	rear = (rear + 1) / BUFSIZ;
+	size++;
+}
+
+int dequeue()
+{
+	int	item;
+	
+	if (empty())
+		return INT_MIN;
+	item = buf[front];
+	front = (front + 1)/ BUFSIZ;
+	size--;
+	return item;
+}
+	
