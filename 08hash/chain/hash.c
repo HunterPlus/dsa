d39309b6389e0182e0046b132e56c041ba71255e
HunterPlus <zlw08140957@gmail.com>
2022-06-14 13:41:17 +0800

    Update hash.c

--- 08hash/chain/hash.c
+++ 08hash/chain/hash.c
@@ -7,10 +7,24 @@ static int hash(int x)
 	return x % HSIZE;
 }
 
+struct record *get(int key)
+{
+	int	i;
+	struct record *rec;
+	
+	i = hash(key);
+	for (rec = hastab[i]; rec != NULL; rec = rec->next)
+		if (rec->key == key)
+			break;
+	return rec;	
+}
+
 void put(struct record *rec)
 {
 	int	i;
 	
+	if (get(rec->key) != NULL)
+		return;
 	i = hash(rec->key);
 	rec->next = hashtab[i];
 	hashtab[i] = rec;
