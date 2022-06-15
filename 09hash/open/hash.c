d2aa74cb85d7104f088b7bd6765388106e2a9466
HunterPlus <zlw08140957@gmail.com>
2022-06-15 10:39:52 +0800

    Update hash.c

--- 09hash/open/hash.c
+++ 09hash/open/hash.c
@@ -11,3 +11,78 @@ static int match(struct entry *ent, char *key, int len)
 	return ent->key && ent->key != TOMBSTONE &&
 		ent->len == len && memcmp(ent->key, key, len) == 0;
 }
+
+static struct entry *get_entry(char *key, int len)
+{
+	struct entry *ent;
+	int 	h;
+	
+	h = hash(key, len);	
+	for (int i = 0; i < HSIZE; i++) {
+		ent = hashtab + (h + i) % HSIZE;
+		if (match(ent, key, len))
+			return ent;
+		if (ent->key == NULL)
+			return NULL;
+	}
+	assert(0);
+}
+
+static struct entry *put_entry(char *key, int len)
+{
+	struct entry *ent;
+	int	h;
+	
+	h = hash(key, len);
+	for (int i = 0; i < HSIZE; i++) {
+		ent = hashtab + (h + i) % HSIZE;
+		if (match(ent, key, len))
+			return ent;
+		if (ent->key == TOMBSTONE) {
+			ent->key = key;
+			ent->len = len;
+			return ent;
+		}
+		if (ent->key == NULL) {
+			ent->key = key;
+			ent->len = len;
+			used++;
+			return ent;
+		}
+	}
+	assert(0);
+}
+
+void *get(char *key)
+{
+	return get2(key, strlen(key));
+}
+
+void *get2(char *key, int len)
+{
+	struct entry *ent = get_entry(key, len);
+	return ent ? ent->rec : NULL;
+}
+
+void put(char *key, void *rec)
+{
+	put2(key, strlen(key), rec);
+}
+
+void put2(char *key, int len, void *rec)
+{
+	struct entry *ent = put_entry(char *key, int len);
+	ent->rec = rec;
+}
+
+void delete(char *key)
+{
+	delete2(key, strlen(key));
+}
+
+void delete2(char *key, int len)
+{
+	struct entry *ent = get_entry(key, len);
+	if(ent)			/* we should free this deleted record */
+		ent->key = TOMBSTONE;
+}
