8cc903a89d138c4171905e082c0eb1d121c7ad45
HunterPlus <zlw08140957@gmail.com>
2022-06-28 09:16:01 +0800

    Create queue.c

---
+++ 14graph/bfs/queue.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+static int buf[BUFSIZ];
+static int _front, _rear, _size;
+
+int empty()
+{
+	return _size == 0;
+}
+
+void enqueue(int data)
+{
+	if (_size == BUFSIZ)
+		return;
+	buf[_rear] = data;
+	_rear = (_rear + 1) % BUFSIZ;
+	_size++;
+}
+
+int dequeue()
+{
+	int	item;
+	
+	if (_size == 0)
+		return INT_MIN;
+	item = buf[_front];
+	_front = (_front + 1) % BUFSIZ;
+	_size--;
+	return item;
+}
