b5f6b8daade853df6f8a92b5355dde3ffb5aa430
HunterPlus <zlw08140957@gmail.com>
2022-06-29 10:30:25 +0800

    Create floyd.c

---
+++ 14graph/floyd.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define INF 99999
+#define n 4
+
+
+void floyd(int g[][n])
+{
+	int	i, j, k;
+	int	dist[n][n];
+	
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+			dist[i][j] = g[i][j];
+	for (k = 0; k < n; k++)
+		for (i = 0; k < n; i++)
+			for (j = 0; j < n; j++) 
+				if (dist[i][k] + dist[k][j] < dist[i][j])
+					dist[i][j] = dist[i][k] + dist[k][j];
+	/* print dist matrix */
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) 
+			if (dist[i][j] = INF)
+				printf("%7s", "INF");
+			else
+				printf("%7d", dist[i][j]);
+		printf("\n");
+	}
+}
+
+/* 
+            10
+       (0)------->(3)
+        |         /|\
+      5 |          |
+        |          | 1
+       \|/         |
+       (1)------->(2)
+            3           	*/
+int main()
+{
+    	int graph[n][n] = { 	{0,   5,  INF, 10},
+                        	{INF, 0,   3, INF},
+                        	{INF, INF, 0,   1},
+                        	{INF, INF, INF, 0}	};
+     
+	floyd(graph);
+    	return 0;
+}
