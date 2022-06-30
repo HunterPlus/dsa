dcd4823577c37cc2aa46d9fd92cc956c65a9bb8e
HunterPlus <zlw08140957@gmail.com>
2022-06-30 14:00:25 +0800

    Update kahn.h

--- 14graph/topological/kahn.h
+++ 14graph/topological/kahn.h
@@ -1,2 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
+struct node {
+        int     dest;
+        struct node *next;
+};
+/* adjacency list graph */
+struct node **creategraph(int);
+void addedge(struct node **, int, int);
+
+/* queue */
+int empty(void);
+void enqueue(int);
+int dequeue(void);
