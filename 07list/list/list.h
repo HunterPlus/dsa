c2836605a967b9567028ab900f5eb65f4a8b4139
HunterPlus <zlw08140957@gmail.com>
2022-09-14 17:57:16 +0800

    Update list.h

--- 07list/list/list.h
+++ 07list/list/list.h
@@ -13,6 +13,7 @@ struct node *push1(struct node *, int);
 void insert(struct node *, int);
 void append(struct node **, int);
 void delete(struct node **, int);
+void delete1(struct node **, int);
 struct node *del(struct node *, int);
 void *toarray(struct node *);
 int listlen(struct node *);
